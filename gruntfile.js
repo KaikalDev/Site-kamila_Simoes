77aa7bae400cfb158704f19feb1f07ae2759a81e
Kaique Luan <kaiquesimoes58@gmail.com>
2024-04-05 12:23:29 -0300

    adição do imagemin

--- gruntfile.js
+++ gruntfile.js
@@ -44,7 +44,51 @@ module.exports = function(grunt){
                         {
                             match: 'ENDERECO_DO_JS',
                             replacement: '../src/scripts/main.js'
-                        }
+                        },
+                        {
+                            match: 'LOGO',
+                            replacement: '/dev/image/logo.PNG'
+                        },
+                        {
+                            match: 'LOGO_MARRON',
+                            replacement: '/dev/image/logo-marron.PNG'
+                        },
+                        {
+                            match: 'LOGO_COMPLETA',
+                            replacement: '/dev/image/logo-completa.PNG'
+                        },
+                        {
+                            match: 'IMAGEM_SOBRE',
+                            replacement: '/dev/image/foto-sobre.jpeg'
+                        },
+                        {
+                            match: 'IMAGEM_CONTATO',
+                            replacement: '/dev/image/foto-contato.jpeg'
+                        },
+                        {
+                            match: 'PROJETO_QUARTO',
+                            replacement: '/dev/image/quarto.jpeg'
+                        },
+                        {
+                            match: 'PROJETO_SALA',
+                            replacement: '/dev/image/sala.jpeg'
+                        },
+                        {
+                            match: 'PROJETO_GURMET',
+                            replacement: '/dev/image/gurmet.jpeg'
+                        },
+                        {
+                            match: 'PROJETO_COMERCIAL',
+                            replacement: '/dev/image/comercial.jpeg'
+                        },
+                        {
+                            match: 'PROJETO_COZINHA',
+                            replacement: '/dev/image/cozinha.jpeg'
+                        },
+                        {
+                            match: 'PROJETO_BANHEIRO',
+                            replacement: '/dev/image/banheiro.JPEG'
+                        },
                     ]
                 },
                 files: [
@@ -76,7 +120,51 @@ module.exports = function(grunt){
                         {
                             match: 'ENDERECO_DO_JS',
                             replacement: './scripts/main.min.js'
-                        }
+                        },
+                        {
+                            match: 'LOGO',
+                            replacement: '/dist/image/logo.PNG'
+                        },
+                        {
+                            match: 'LOGO_MARRON',
+                            replacement: '/dist/image/logo-marron.PNG'
+                        },
+                        {
+                            match: 'LOGO_COMPLETA',
+                            replacement: '/dist/image/logo-completa.PNG'
+                        },
+                        {
+                            match: 'IMAGEM_SOBRE',
+                            replacement: '/dist/image/foto-sobre.jpeg'
+                        },
+                        {
+                            match: 'IMAGEM_CONTATO',
+                            replacement: '/dist/image/foto-contato.jpeg'
+                        },
+                        {
+                            match: 'PROJETO_QUARTO',
+                            replacement: '/dist/image/quarto.jpeg'
+                        },
+                        {
+                            match: 'PROJETO_SALA',
+                            replacement: '/dist/image/sala.jpeg'
+                        },
+                        {
+                            match: 'PROJETO_GURMET',
+                            replacement: '/dist/image/gurmet.jpeg'
+                        },
+                        {
+                            match: 'PROJETO_COMERCIAL',
+                            replacement: '/dist/image/comercial.jpeg'
+                        },
+                        {
+                            match: 'PROJETO_COZINHA',
+                            replacement: '/dist/image/cozinha.jpeg'
+                        },
+                        {
+                            match: 'PROJETO_BANHEIRO',
+                            replacement: '/dist/image/banheiro.JPEG'
+                        },
                     ]
                 },
                 files: [
@@ -107,6 +195,27 @@ module.exports = function(grunt){
                 }
             }
         },
+        imagemin: {
+			dist: {
+				files: [{
+					expand: true,
+					cwd: 'src/',
+					src: '**/*.{JPEG,jpeg,PNG}',
+					dest: 'dist/',
+					dest: 'dev/'
+				}]
+			},
+			rename: {
+				files: {
+					'dist/image/*.JPEG': 'src/image/*.JPEG',
+					'dist/image/*.jpeg': 'src/image/*.jpeg',
+					'dist/image/*.PNG': 'src/image/*.PNG',
+                    'dev/image/*.JPEG': 'src/image/*.JPEG',
+					'dev/image/*.jpeg': 'src/image/*.jpeg',
+					'dev/image/*.PNG': 'src/image/*.PNG'
+				}
+			}
+		},
         clean: ['prebuild'],
         uglify: {
             traget: {
@@ -123,7 +232,8 @@ module.exports = function(grunt){
     grunt.loadNpmTasks('grunt-contrib-htmlmin');
     grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-imagemin');
 
     grunt.registerTask('default',['watch'])
-    grunt.registerTask('build',['less:production','htmlmin:dist', 'replace:dist','clean', 'uglify'])
+    grunt.registerTask('build',['less:production','htmlmin:dist', 'replace:dist','clean', 'uglify','imagemin'])
 }
